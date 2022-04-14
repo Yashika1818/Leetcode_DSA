aa58b1d8174a9765e9bedcc5228e3c363cfddb64
Yashika1818 <79274294+Yashika1818@users.noreply.github.com>
2022-04-14 18:51:40 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (85.64%) - LeetHub

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,16 +1,22 @@
 class Solution {
-    
-    public double myPow(double x, int n) {
-        return n>0 ? pow(x,n) : 1.0/pow(x,-n);
-    }
-    
-    public double pow(double x, int n){
-        
-        if(n==0){
-            return 1.0;
-        }else{
-            double half=pow(x,n/2);
-            return n%2==0 ? half*half : half*half*x;
+      public double myPow(double x, int n){
+          double ans=myans(x,n);
+          if(n<0)return (1/ans);
+          return ans;
+      }             
+    public double myans(double x, int n) {
+        if(n==0)return 1.00;
+         double ans=0;
+     
+       ans=myans(x,n/2);
+        if(n%2==0)
+      return ans*ans;
+        else if(n%2!=0){
+      
+            return ans*ans*x;
         }
+           
+
+        return 0;
     }
 }
