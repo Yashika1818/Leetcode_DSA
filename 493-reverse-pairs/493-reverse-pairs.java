ec62284d9cdbec182a9af3b8131c5c14ad577669
Yashika1818 <79274294+Yashika1818@users.noreply.github.com>
2022-04-16 14:14:41 +0530

    Time: 175 ms (22.24%), Space: 119.4 MB (12.99%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.java
+++ 493-reverse-pairs/493-reverse-pairs.java
@@ -1,63 +1,50 @@
 import java.util.Arrays;
 class Solution {
     //we can't directly implement merge sort here bcoz if left[i]>right[j] , we do inv+ nd j++, i.e we have included all the ith after that in inv+ but in this case we do inv+ only when left[i]>(2*right[j]) this is satisfied , it means all the ith elements after the curr i value are considered nd we are moving to j++ to check for next j with the curr i but this is not the case, suppose left[i]!>(2*right[j]) for current ith but for next ith element , it is poss for this jth but due to increment in this jth , we are ignoring this case and hence this algo is wrong.
-    public int reversePairs(int[] nums) {
-        // for(int num:nums){
-        // System.out.println(num);
-        // }
-        // long[] numf=new long[nums.length];
-        // for(int i=0;i<nums.length;i++){
-        //     numf[i]=(long)nums[i];
-        // }
-        return mergeSortAndCount(nums,0,nums.length-1);
-        
-    }
-    public int mergeAndCount(int[] nums,int l,int m,int r){
-        int[] left=Arrays.copyOfRange(nums,l,m+1);
-        int[] right=Arrays.copyOfRange(nums,m+1,r+1);
-        int i=0,j=0,k=l;
-        int inv=0;
-        
-         while(i<left.length && j<right.length){
-            if(left[i]<=(2*(long)right[j]))i++;
-            else if(left[i]>2*(long)right[j])
-            {
-               inv+=left.length-i;
+  
+    static int merge(int[] nums, int low, int mid, int high) {
+        int cnt = 0;
+        int j = mid + 1; 
+        for(int i = low;i<=mid;i++) {
+            while(j<=high && nums[i] > (2 * (long) nums[j])) {
                 j++;
-                
             }
+            cnt += (j - (mid+1));
         }
-        i=0;
-        j=0;
-        while(i<left.length && j<right.length){
-            if(left[i]<=right[j])nums[k++]=left[i++];
-            else if(left[i]>right[j])
-            {
-              
-                nums[k++]=right[j++];
-               
+        
+        ArrayList<Integer> temp = new ArrayList<>(); 
+        int left = low, right = mid+1; 
+        while(left <= mid && right<=high) {
+            if(nums[left]<=nums[right]) {
+                temp.add(nums[left++]); 
+            }
+            else {
+                temp.add(nums[right++]); 
             }
         }
         
-        while(i<left.length){
-            nums[k++]=left[i++];
+        while(left<=mid) {
+            temp.add(nums[left++]); 
         }
-        while(j<right.length){
-            nums[k++]=right[j++];
+        while(right<=high) {
+            temp.add(nums[right++]); 
         }
-      
         
-        return inv;
-    }
-    public int mergeSortAndCount(int[] nums,int l,int r){
-        int count=0;
-        if(l<r){
-            int m=(l+r)/2;
-            //left
-            count+=mergeSortAndCount(nums,l,m);
-            count+=mergeSortAndCount(nums,m+1,r);
-            count+=mergeAndCount(nums,l,m,r);
+        for(int i = low; i<=high;i++) {
+            nums[i] = temp.get(i - low); 
         }
-        return count;
+        return cnt; 
     }
+    static int mergeSort(int[] nums, int low, int high) {
+        if(low>=high) return 0; 
+        int mid = (low + high) / 2;
+        int inv = mergeSort(nums, low, mid); 
+        inv += mergeSort(nums, mid+1, high); 
+        inv += merge(nums, low, mid, high); 
+        return inv; 
+    }
+   static int reversePairs(int[] nums) {
+        return mergeSort(nums, 0, nums.length - 1); 
+    }
+
 }
