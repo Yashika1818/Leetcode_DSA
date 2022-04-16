f3d6806a63b14ae728e598e07f8c9bb456739ef4
Yashika1818 <79274294+Yashika1818@users.noreply.github.com>
2022-04-16 15:12:36 +0530

    Time: 1 ms (99.71%), Space: 42.1 MB (97.48%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        // we need to apply loop for each nums[i] , becoz if nums[i]>target it may be the case that with a negative element , it becomes equal to target.
         int ans[]=new int[2];
         HashMap<Integer,Integer> hm=new HashMap<>();
         for(int i=0;i<nums.length;i++){
