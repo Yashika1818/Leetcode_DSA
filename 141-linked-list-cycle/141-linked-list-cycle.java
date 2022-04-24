2d8b5024ba9543c5dfe77f14aa728f400c66926e
Yashika1818 <79274294+Yashika1818@users.noreply.github.com>
2022-04-24 22:04:30 +0530

    Time: 0 ms (100.00%), Space: 46.7 MB (15.71%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.java
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -11,17 +11,15 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        HashMap<ListNode,Boolean> hm=new HashMap<>(); //hashmap will contain the reference/address of all the nodes of list bcz of tmp pointer.
-        ListNode tmp=head; //here tmp is a pointer of type listnode which points to head . It is not the exact / original node present in the list.
-        boolean chck=true;
-        while(!hm.containsKey(tmp)){
-            if(tmp==null){
-                chck=false;
-                break;
-            }
-            hm.put(tmp,true);
-            tmp=tmp.next;
+        ListNode slow=head;
+        ListNode fast=head;
+     
+        while(fast!=null && fast.next!=null){ // this condition is when there's no loop
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast)return true; // when there's a loop
         }
-        return chck;
+        
+        return false;
     }
 }
