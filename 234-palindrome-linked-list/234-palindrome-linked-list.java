75db4da2fe2966eea6ff658e9e75d849d55c4482
Yashika1818 <79274294+Yashika1818@users.noreply.github.com>
2022-04-25 19:36:19 +0530

    Time: 7 ms (59.43%), Space: 96.9 MB (64.73%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -10,34 +10,35 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        //using fast and slow pointers
+        if(head==null)return true;
         ListNode slow=head;
         ListNode fast=head;
+        
         while(fast.next!=null && fast.next.next!=null){
             fast=fast.next.next;
-            slow=slow.next;  
-        }
-         slow=slow.next; //start pointer of second half which is to be reversed
-        slow=reverse(slow);
-        fast=head;
-        while(slow!=null){ //slow has lesser length as compared to that of fast ll
-            if(fast.val!=slow.val){
-                return false;
-            }
-            fast=fast.next;
             slow=slow.next;
+        }
+        slow=slow.next;
+       slow= reverse(slow);
+        fast=head;
+        while(slow!=null){
+            if(slow.val!=fast.val)return false;
+            slow=slow.next;
+            fast=fast.next;
         }
         return true;
     }
-    ListNode reverse(ListNode head){
-        ListNode curr=head;
-        ListNode prev=null;
-        while(curr!=null){
-            ListNode temp=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=temp;
+    
+    public ListNode reverse(ListNode head){
+        ListNode tmp=head;
+        ListNode rev=null;
+        while(tmp!=null){
+            ListNode dummy=tmp.next;
+            tmp.next=rev;
+            rev=tmp;
+            tmp=dummy;
         }
-        return prev;
+        
+        return rev;
     }
 }
