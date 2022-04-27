270feddbc8fd0fd7d5e62e9a1ef020736d00e969
Yashika1818 <79274294+Yashika1818@users.noreply.github.com>
2022-04-27 19:21:23 +0530

    Time: 25 ms (81.24%), Space: 59.4 MB (34.72%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,16 +1,16 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        if(nums.length==0){
-             List<List<Integer>> ans=new ArrayList<List<Integer>>();
-            return ans;
-        }
-        if(nums.length<3){
-           List<List<Integer>> ans=new ArrayList<List<Integer>>();
-            return ans;
-        }
+        // if(nums.length==0){
+        //      List<List<Integer>> ans=new ArrayList<List<Integer>>();
+        //     return ans;
+        // }
+        // if(nums.length<3){
+        //    List<List<Integer>> ans=new ArrayList<List<Integer>>();
+        //     return ans;
+        // }
         Arrays.sort(nums);
         int sum=0;
-        List<List<Integer>> ans=new ArrayList<List<Integer>>();
+        List<List<Integer>> ans=new LinkedList<>();
         for(int i=0;i<nums.length-1;i++){
             if(i>0 && nums[i]==nums[i-1]){
                 continue;
@@ -19,25 +19,30 @@ class Solution {
             int start=i+1;
             int end=nums.length-1;
             while(start<end){
-                if(start>i+1 && nums[start]==nums[start-1])
-                {
-                    start++;
-                    continue;
-                }
-                if(end< nums.length-1 && nums[end]==nums[end+1]){
-                    end--;
-                    continue;
-                }
+                //WE NEED NOT TO APPLY THESE CONDITIONS FOR EVERY ITERATION
+                //BCZ WE ARE DOING SO TO REMOVE DUPLICATES, ANS IS CREATED WHEN ELSE CONDITION IS EXECUTED , THEREFORE WE CAN ONLY WRITE THESE COONDITION IN ELSE .
+                // if(start>i+1 && nums[start]==nums[start-1])
+                // {
+                //     start++;
+                //     continue;
+                // }
+                // if(end< nums.length-1 && nums[end]==nums[end+1]){
+                //     end--;
+                //     continue;
+                // }
             if(nums[start]+nums[end] < sum ){
                 start++;
             }else if(nums[start]+nums[end] > sum ){
                 end--;
             }else{
-                List<Integer> l=new ArrayList<>();
-                l.add(nums[i]);
-                l.add(nums[start]);
-                l.add(nums[end]);
-                ans.add(l);
+                ans.add(Arrays.asList(nums[i],nums[start],nums[end]));
+                // List<Integer> l=new ArrayList<>();
+                // l.add(nums[i]);
+                // l.add(nums[start]);
+                // l.add(nums[end]);
+                // ans.add(l);
+                while(start<end && nums[start]==nums[start+1])start++; //jaise e start>end hojaega kuch krne ki need nhi hai.
+                while(start<end && nums[end]==nums[end-1])end--;
                 start++;
                 end--;
             }
