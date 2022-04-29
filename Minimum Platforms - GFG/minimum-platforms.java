4f3e1d59fb01b7c6a19d59d808bcd37f847c2795
Yashika1818 <79274294+Yashika1818@users.noreply.github.com>
2022-04-29 12:03:17 +0530

    Added solution - LeetHub

---
+++ Minimum Platforms - GFG/minimum-platforms.java
@@ -0,0 +1,72 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        
+        while(t-- > 0)
+        {
+            String str[] = read.readLine().trim().split(" ");
+            int n = Integer.parseInt(str[0]);
+            
+            int arr[] = new int[n];
+            int dep[] = new int[n];
+            
+            str = read.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+              arr[i] = Integer.parseInt(str[i]);
+            str = read.readLine().trim().split(" ");
+            for(int i = 0; i < n; i++)
+                dep[i] = Integer.parseInt(str[i]);
+                
+            System.out.println(new Solution().findPlatform(arr, dep, n));
+        }
+    }
+    
+    
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution
+{
+    //Function to find the minimum number of platforms required at the
+    //railway station such that no train waits.
+    static int findPlatform(int arr[], int dep[], int n)
+    {
+        // add your code here
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        
+        int min_platforms=1;
+        int needed_plat=1;
+        int i=1;
+        int j=0;
+        while(i<arr.length && j<dep.length){
+            if(arr[i]<=dep[j]){
+                needed_plat++;
+                i++;
+            }else{
+                needed_plat--;
+                j++;
+            }
+            if(needed_plat>min_platforms)
+            min_platforms=needed_plat;
+        }
+        return min_platforms;
+    }
+    
+}
+
