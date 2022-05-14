e5e1e44d70c0f1737e9fb957d4fe16615c9cc143
Yashika1818 <79274294+Yashika1818@users.noreply.github.com>
2022-05-14 17:07:14 +0530

    Time: 2 ms (82.67%), Space: 40.9 MB (71.28%) - LeetHub

---
+++ 60-permutation-sequence/60-permutation-sequence.java
@@ -0,0 +1,29 @@
+class Solution {
+    public String getPermutation(int n, int k) {
+        k=k-1;
+        int fact=1;
+        StringBuilder  s=new StringBuilder();
+        StringBuilder ans=new StringBuilder();
+        for(int i=1;i<=n;i++){
+            fact*=i;
+            s.append(i);
+        }
+       
+        while(s.length()!=1){
+             fact=fact/n;
+            int remove=k/fact;
+            ans.append(s.charAt(remove));
+             k=k%fact;
+            
+            n=n-1;
+          //  int rem=Integer.parseInt((s.charAt(remove)));
+            
+            
+            s.delete(remove,remove+1);
+           
+        }
+        ans.append(s.charAt(0));
+        
+        return ans.toString();
+    }
+}
