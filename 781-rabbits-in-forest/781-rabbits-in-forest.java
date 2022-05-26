4989e40509c0e9ec0bed5ad491d483bf4fc37dd5
Yashika1818 <79274294+Yashika1818@users.noreply.github.com>
2022-05-26 20:30:34 +0530

    Time: 3 ms (80.85%), Space: 41.7 MB (79.67%) - LeetHub

--- 781-rabbits-in-forest/781-rabbits-in-forest.java
+++ 781-rabbits-in-forest/781-rabbits-in-forest.java
@@ -10,20 +10,16 @@ class Solution {
         }
         int count=0;
         for(int keys:hm.keySet()){
-            if(keys==0){
-                count+=hm.get(0);
-                continue;
-            }
+           
             int n1=hm.get(keys);
             int n2=keys+1;
             
-            if(n1>n2){
+            
             int rem=n1%n2;
             int quot=n1/n2;
            // count+=(rem>0 ? n2 : 0)+(quot*n2);  //floor + something = ceil
                 count+=(int)Math.ceil((n1*1.0)/(n2*1.0))*n2;
-            }else
-                count+=keys+1;
+           
         }
         return count;
     }
