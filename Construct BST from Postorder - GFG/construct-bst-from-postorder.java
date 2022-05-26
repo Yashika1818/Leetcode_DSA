1626c5f2b832cb7ab2ad475db7deb1ff271993f5
Yashika1818 <79274294+Yashika1818@users.noreply.github.com>
2022-05-26 13:10:42 +0530

    Added solution - LeetHub

---
+++ Construct BST from Postorder - GFG/construct-bst-from-postorder.java
@@ -0,0 +1,82 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.Scanner;
+import java.lang.Math;
+import java.io.*;
+class Node{
+    int data;
+    Node left,right;
+    Node(int d)
+    {
+        data=d;
+        left=right=null;
+    }
+}
+class GFG2
+{
+    public static void inorder(Node root)
+    {
+        if(root==null)
+        return;
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        while(t-->0)
+        {
+            int n=sc.nextInt();
+            int []post=new int[n];
+            for(int i=0;i<n;i++)
+            {
+                int b=sc.nextInt();
+                post[i]=b;
+            }
+            GFG obj=new GFG();
+            Node root=obj.constructTree(post,n);
+            inorder(root);
+            System.out.println();
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+/*class Node{
+    int data;
+    Node left,right;
+    Node(int d)
+    {
+        data=d;
+        left=right=null;
+    }
+}*/
+
+// Function should return the root of the bst formed using postorder traversal.
+class GFG
+{
+    static int idx=0;
+    public static Node constructTree(int post[],int n)
+    {
+        //Add your code here.
+        int lb=Integer.MIN_VALUE;
+        idx=n-1;
+        Node root=helper(post,n,lb);
+        return root;
+    }
+    public static Node helper(int[] post,int n,int lb){
+        if(idx<0 || post[idx]<lb){
+            return null;
+        }
+        Node root=new Node(post[idx]);
+        idx--;
+        root.right=helper(post,n,root.data);
+        root.left=helper(post,n,lb);
+        return root;
+    }
+}
