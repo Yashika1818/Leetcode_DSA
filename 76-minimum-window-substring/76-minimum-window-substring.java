fdf2e563a97cd295283ce2b7d3c042037ec893a5
Yashika1818 <79274294+Yashika1818@users.noreply.github.com>
2022-05-28 11:50:24 +0530

    Time: 42 ms (18.55%), Space: 51.1 MB (18.97%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.java
@@ -0,0 +1,61 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if(s.length()<t.length())return "";
+        int[] map1=new int[123];
+        int[] map2=new int[123];
+        for(int i=0;i<t.length();i++){
+            char c=t.charAt(i);
+         map1[c]++;
+           
+        }
+        
+        int i=0;
+        int j=0;
+        int min=Integer.MAX_VALUE;
+        String minwin="";
+        while(j<s.length()){
+            
+           
+               
+                char c=s.charAt(j);
+                  map2[c]++;
+            
+               
+                j++;
+            
+            
+            // int len=j-i;
+            // if(equals(map1,map2)==true && len<min){min=len;
+            //         minwin=s.substring(i,j);
+            //            }
+         
+            while(i<s.length() && equals(map1,map2)==true){
+               int len=j-i;
+                  if(len<min){
+               min=len;
+                    minwin=s.substring(i,j); 
+               }
+                char ch=s.charAt(i);
+                 map2[ch]--;
+            
+                 
+          
+             
+                i++;
+                
+            }
+           
+        }
+        return minwin;
+    }
+    public boolean equals(int[] map1,int[] map2){
+        for(int i=0;i<123;i++){
+            // agr map1[i]==0 h toh no issue else check
+            if(map1[i]==0)continue;
+            else{
+                if(map1[i]>map2[i])return false;
+            }
+        }
+        return true;
+    }
+}
