6d57b3a7367c87fa5db85fec919914a9ff7e8b47
Yashika Goel <79274294+Yashika1818@users.noreply.github.com>
2022-06-07 23:52:37 +0530

    Added solution - LeetHub

---
+++ Lowest Common Ancestor in a Binary Tree - GFG/lowest-common-ancestor-in-a-binary-tree.java
@@ -0,0 +1,161 @@
+// { Driver Code Starts
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t-- > 0){
+	            String input[] = br.readLine().trim().split(" ");
+                int a = Integer.parseInt(input[0]);
+                int b = Integer.parseInt(input[1]);
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+                Solution g = new Solution();
+                Node k = g.lca(root,a,b);
+    			System.out.println(k.data);
+    	        
+	        }
+	}
+}
+// } Driver Code Ends
+
+
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}*/
+
+class Solution
+{
+    //Function to return the lowest common ancestor in a Binary Tree.
+    boolean f1=false;
+    boolean f2=false;
+    	Node lca(Node root, int n1,int n2)
+	{
+	    
+	    Node l=helper(root,n1,n2);
+	   if(f1==true && f2==true)
+	    return l;
+	   // else
+	   // return null;
+	   return null;
+	}
+	
+	Node helper(Node root, int n1,int n2)
+	{
+		// Your code here
+		if(root==null)return null;
+		
+		Node left=helper(root.left,n1,n2);
+		Node right=helper(root.right,n1,n2);
+		
+		if(root.data==n1){
+		    f1=true;
+		    return root;
+		}
+		else if(root.data==n2){
+		    f2=true;
+		    return root;
+		}
+		else if(left!=null && right!=null)return root;
+		else if(left!=null)return left;
+		else if(right!=null)return right;
+		else
+		return null;
+	}
+
+}
+
