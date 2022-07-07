61612957a9d094ff6928b441b4fafcaaff76af74
Yashika Goel <79274294+Yashika1818@users.noreply.github.com>
2022-07-07 18:12:16 +0530

    Time: 2 ms (88.75%), Space: 42.3 MB (64.59%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,4 +1,4 @@
-//Memoization --> Brute force
+//Memoization(optimized)
 
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
@@ -9,16 +9,17 @@ class Solution {
             return s1.equals(s3);
         }
         if(s1.length()+s2.length() !=s3.length())return false;
-        int[][][] dp=new int[s1.length()][s2.length()][s3.length()];
-        for(int[][] d:dp){
-            for(int[] row:d){
+        int[][] dp=new int[s1.length()][s2.length()];
+      
+            for(int[] row:dp){
                 Arrays.fill(row,-1);
             }
-        }
-        return helper(s1,s2,s3,0,0,0,dp);
+        
+        k=0;
+        return helper(s1,s2,s3,0,0,dp);
     }
-    
-    public boolean helper(String s1,String s2,String s3,int i,int j,int k,int[][][] dp){
+    int k=0;
+    public boolean helper(String s1,String s2,String s3,int i,int j,int[][] dp){
         if(i==s1.length() &&  j<s2.length()){
           
             if((s2.substring(j)).equals(s3.substring(k))){
@@ -36,21 +37,23 @@ class Solution {
             
             
         }
-        if(dp[i][j][k]!=-1)return dp[i][j][k]==1 ? true :false;
+        if(dp[i][j]!=-1)return dp[i][j]==1 ? true :false;
         
         boolean ans=false;
            if(s2.charAt(j)==s3.charAt(k)){
-         
-            ans=ans | helper(s1,s2,s3,i,j+1,k+1,dp);
+            k=k+1;
+            ans=ans | helper(s1,s2,s3,i,j+1,dp);
+            k=k-1;
         }
         if(s1.charAt(i)==s3.charAt(k)){
-       
-             ans|=helper(s1,s2,s3,i+1,j,k+1,dp) ;
+         k=k+1;
+             ans|=helper(s1,s2,s3,i+1,j,dp) ;
+              k=k-1;
         }
          if(ans==true){
-             dp[i][j][k] = 1;
+             dp[i][j] = 1;
          }else{
-              dp[i][j][k] = 0;
+              dp[i][j] = 0;
          }
         return  ans;
     }
