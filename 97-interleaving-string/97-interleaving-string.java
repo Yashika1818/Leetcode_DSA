2574f7d7b58ce9453be1a9d798ce2736b513f93f
Yashika Goel <79274294+Yashika1818@users.noreply.github.com>
2022-07-07 21:16:49 +0530

    Time: 11 ms (22.18%), Space: 43.1 MB (19.51%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.java
+++ 97-interleaving-string/97-interleaving-string.java
@@ -1,4 +1,4 @@
-//Memoization(optimized)
+//Tabulation 2D(optimized)
 
 class Solution {
     public boolean isInterleave(String s1, String s2, String s3) {
@@ -9,53 +9,34 @@ class Solution {
             return s1.equals(s3);
         }
         if(s1.length()+s2.length() !=s3.length())return false;
-        int[][] dp=new int[s1.length()][s2.length()];
-      
-            for(int[] row:dp){
-                Arrays.fill(row,-1);
-            }
-        
-        k=0;
-        return helper(s1,s2,s3,0,0,dp);
-    }
-    int k=0;
-    public boolean helper(String s1,String s2,String s3,int i,int j,int[][] dp){
-        if(i==s1.length() &&  j<s2.length()){
+        boolean[][] dp=new boolean[s1.length()+1][s2.length()+1];
+      dp[s1.length()][s2.length()]=true;
+       for(int j=0;j<=s2.length();j++){
+           dp[s1.length()][j]= s2.substring(j).equals(s3.substring(s1.length()+j)) ;
+       }
+       for(int i=0;i<=s1.length();i++){
+           dp[i][s2.length()]= s1.substring(i).equals(s3.substring(i+s2.length())) ;
+       }
+   
+        for(int i=s1.length()-1;i>=0;i--){
+            for(int j=s2.length()-1;j>=0;j--){
+                 boolean ans=false;
+           if(s2.charAt(j)==s3.charAt(i+j)){
           
-            if((s2.substring(j)).equals(s3.substring(k))){
-                
-                return true;
+            ans=ans | dp[i][j+1];
+           
+           }
+           if(s1.charAt(i)==s3.charAt(i+j)){
+           
+             ans|=dp[i+1][j] ;
+             
             }
-            
-            return false;
+                
+                dp[i][j]=ans;
+            }
         }
-         if(j==s2.length() && i<s1.length()){
-         
-         if(s1.substring(i).equals(s3.substring(k)))return true;
-            
-            return false;         
-            
-            
-        }
-        if(dp[i][j]!=-1)return dp[i][j]==1 ? true :false;
-        
-        boolean ans=false;
-           if(s2.charAt(j)==s3.charAt(k)){
-            k=k+1;
-            ans=ans | helper(s1,s2,s3,i,j+1,dp);
-            k=k-1;
-        }
-        if(s1.charAt(i)==s3.charAt(k)){
-         k=k+1;
-             ans|=helper(s1,s2,s3,i+1,j,dp) ;
-              k=k-1;
-        }
-         if(ans==true){
-             dp[i][j] = 1;
-         }else{
-              dp[i][j] = 0;
-         }
-        return  ans;
+ 
+        return dp[0][0];
     }
-    
+  
 }
