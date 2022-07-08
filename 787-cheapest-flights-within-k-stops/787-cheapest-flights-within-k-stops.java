6b954bca1b15a0ac7c24d3a0f130f93ad3df8057
Yashika Goel <79274294+Yashika1818@users.noreply.github.com>
2022-07-08 23:41:43 +0530

    Time: 3 ms (99.50%), Space: 43.6 MB (87.65%) - LeetHub

---
+++ 787-cheapest-flights-within-k-stops/787-cheapest-flights-within-k-stops.java
@@ -0,0 +1,37 @@
+class Solution {
+    class Pair{
+        int nbr;
+        int wt;
+        Pair(int nbr,int wt){
+            this.nbr=nbr;
+            this.wt=wt;
+        }
+    }
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        //make graph
+       
+    int[] d1=new int[n];
+    for(int i=0;i<n;i++){
+        if(i==src)d1[i]=0;
+        else
+            d1[i]=(int)1e9;
+    }
+    int[] d2=new int[n];
+     d2=d1.clone();
+   
+        for(int i=1;i<=k+1;i++){
+        for(int[] f:flights){
+            int start=f[0];
+            int end=f[1];
+            if(d1[start]+f[2] < d2[end]){
+                d2[end]=d1[start]+f[2];
+            }
+        }
+            d1=d2.clone();
+        }
+     
+       
+        if(d2[dst]!=1e9)return d2[dst];
+        return -1;
+    }
+}
