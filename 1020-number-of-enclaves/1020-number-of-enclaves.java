4fe5b1148ac53bae27a72783fb0276f9d9f64383
Yashika Goel <79274294+Yashika1818@users.noreply.github.com>
2022-07-09 00:50:06 +0530

    Time: 13 ms (26.01%), Space: 65.9 MB (40.41%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,43 +1,33 @@
-//NOTE : we can't take cntcell or boolean isTrue inside the call because during backtrack --> everything even their values will be backtracked and result will be same as what we passed initially.
-//therefore , make them global variables.
-
-
 class Solution {
-     boolean isTrue=true;
-     int cntcell=0;
-    
     public int numEnclaves(int[][] grid) {
-        int count=0;
-        boolean[][] visited=new boolean[grid.length][grid[0].length];
-       
+        boolean[][] vis=new boolean[grid.length][grid[0].length];
+        int cnt=0;
         for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-             
-              if(visited[i][j]==false && grid[i][j]==1 ){
-  
-                  cntcell=0;
-                  isTrue=true;
-            traverse(grid,visited,i,j);
- 
-                  if(isTrue==true)count+=cntcell;
-                  
-              }
+            for(int j=0;j<grid[0].length;j++)
+            {
+                count=0;
+                if(grid[i][j]!=0 && vis[i][j]==false){
+                    boolean chck=dfs(grid,vis,i,j);
+                    if(chck)cnt+=count;
+                }
             }
         }
-        return count;
-    }
-    public void traverse(int[][] grid,boolean[][] visited,int i,int j){
-        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length){
-            isTrue=false;
-            return;
-        }else if(visited[i][j]==true || grid[i][j]==0)return;
         
-        visited[i][j]=true;
-      cntcell++;
-        traverse(grid,visited,i,j+1);
-        traverse(grid,visited,i+1,j);
-        traverse(grid,visited,i-1,j);
-        traverse(grid,visited,i,j-1);
-
+        return cnt;
+    }
+    int count=0;
+    public boolean dfs(int[][] grid,boolean[][] vis,int i,int j){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length )return false;
+        
+        if(vis[i][j]==true || grid[i][j]==0)return true;
+        vis[i][j]=true;
+        count++;
+        boolean c1=dfs(grid,vis,i+1,j);
+        boolean c2=dfs(grid,vis,i-1,j);
+        boolean c3=dfs(grid,vis,i,j+1);
+        boolean c4=dfs(grid,vis,i,j-1);
+        if(c1==false || c2==false || c3==false || c4==false)
+            return false;
+        return true;
     }
 }
