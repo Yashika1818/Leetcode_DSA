669fc37ed96b5740b39c6226400676e9dc05b6d1
Yashika Goel <79274294+Yashika1818@users.noreply.github.com>
2022-07-08 19:56:56 +0530

    Time: 7 ms (33.86%), Space: 58.1 MB (14.41%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,30 +1,27 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        boolean[][] visited=new boolean[grid.length][grid[0].length];
-        int n=grid.length;
-        int m=grid[0].length;
-        int count=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1' && visited[i][j]==false){
-                    traverse(grid,visited,i,j);
-                    count++;
+        boolean[][] vis=new boolean[grid.length][grid[0].length];
+        int cnt=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]!='0' && vis[i][j]==false){
+                  dfs(grid,vis,i,j);
+                      cnt++;
                 }
             }
         }
-        return count;
+        return cnt;
     }
     
-    public void traverse(char[][] grid,boolean[][] visited,int i,int j){
-        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || visited[i][j]==true || grid[i][j]=='0' ){
-            return;
-        }
-        visited[i][j]=true;
-        traverse(grid,visited,i,j+1);
-         traverse(grid,visited,i,j-1);
-         traverse(grid,visited,i+1,j);
-         traverse(grid,visited,i-1,j);
+    public void dfs(char[][] grid,boolean[][] vis,int i,int j){
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]=='0' || vis[i][j]==true)return;
         
+        vis[i][j]=true;
+        dfs(grid,vis,i+1,j);
+        dfs(grid,vis,i-1,j);
+        dfs(grid,vis,i,j+1);
+        dfs(grid,vis,i,j-1);
         
+        return;
     }
 }
