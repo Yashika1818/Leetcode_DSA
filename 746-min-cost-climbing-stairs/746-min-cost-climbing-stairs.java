67cd2719a696f21878d067b7833f574e740d7201
Yashika Goel <79274294+Yashika1818@users.noreply.github.com>
2022-07-10 13:32:41 +0530

    Time: 2 ms (32.47%), Space: 43.4 MB (69.76%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -0,0 +1,24 @@
+//recursion
+
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+       int[] dp=new int[cost.length];
+        Arrays.fill(dp,-1);
+    int ans1=helper(cost,0,dp);
+    int ans2=helper(cost,1,dp);
+        
+        return Math.min(ans1,ans2);
+    }
+    
+    public int helper(int[] cost,int i,int[] dp){
+        
+        if(i==cost.length)return 0;
+        if(i>cost.length)return (int)1e9;
+        if(dp[i]!=-1)return dp[i];
+        int mini=(int)1e9;
+        int ans1=cost[i]+helper(cost,i+1,dp);
+        int ans2=cost[i]+helper(cost,i+2,dp);
+        
+        return dp[i]=Math.min(ans1,ans2);
+    }
+}
