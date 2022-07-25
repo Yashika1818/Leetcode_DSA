e465d969a0315cca36965ce334e3a7c711586e14
Yashika Goel <79274294+Yashika1818@users.noreply.github.com>
2022-07-25 20:16:01 +0530

    Time: 0 ms (100.00%), Space: 61.2 MB (9.62%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -3,22 +3,21 @@ class Solution {
         int lo=0;
         int hi=nums.length-1;
         while(lo<=hi){
-         int mid=(lo+hi)/2;
-            if(mid-1>=0 && nums[mid]==nums[mid-1]){
-                if(mid%2!=0){
-                    lo=mid+1;
-                }else{
-                    hi=mid-1;
-                }
-            }else if(mid+1<nums.length && nums[mid]==nums[mid+1]){
-                 if(mid%2!=0){
-                    hi=mid-1;
-                }else{
-                    lo=mid+1;
-                }
-            }else{
+            int mid=(lo+hi)/2;
+            int left= mid-1 >=0 ? (((nums[mid]!=nums[mid-1])==true) ?1 :0) : 1;
+            int right=mid+1<nums.length ? (((nums[mid]!=nums[mid+1])==true) ?1 :0) :1;
+            if(left==1 && right==1) {
                 return nums[mid];
+            }else if(mid%2==0){
+                if(mid-1>=0 && nums[mid-1]==nums[mid])hi=mid-1;
+                else
+                    lo=mid+1;
+            }else{
+                if(mid-1>=0 && nums[mid-1]==nums[mid])lo=mid+1;
+                else
+                    hi=mid-1;
             }
+
         }
         return -1;
     }
