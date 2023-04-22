3fe5c51941b2b1793a5f8f9579a201297710e442
Yashika Goel <79274294+Yashika1818@users.noreply.github.com>
2023-04-22 20:56:42 +0530

    Time: 55 ms (9.07%), Space: 43.7 MB (73.68%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,27 +1,23 @@
-//memoization
 class Solution {
     public int minInsertions(String s) {
-         StringBuilder sb=new StringBuilder(s);
-         String rev=sb.reverse().toString();
-         int[][] dp=new int[s.length()][rev.length()];
-        for(int[] row:dp){
-            Arrays.fill(row,-1);
-        }
-		return s.length()-helper(s,rev,s.length()-1,rev.length()-1,dp);
+      StringBuilder rev=new StringBuilder(s);
+       rev.reverse();
+       String r=rev.toString();
+       int[][] dp=new int[s.length()+1][s.length()+1];
+       for(int[] d:dp){
+           Arrays.fill(d,-1);
+       }
+       return s.length()-sol(s,r,s.length()-1,r.length()-1,dp); 
     }
-    	public static int helper(String s,String r,int i,int j,int[][] dp){
-	    if(i<0 || j<0){
-	        return 0;
-	    }
-            if(dp[i][j]!=-1)return dp[i][j];
-	    int maxi=0;
-	    if(s.charAt(i)==r.charAt(j)){
-	        maxi=1+helper(s,r,i-1,j-1,dp);
-	    }else{
-	        maxi=Math.max(helper(s,r,i-1,j,dp),helper(s,r,i,j-1,dp));
-	    }
-	    
-	    return dp[i][j]=maxi;
-	}
-    
+     public int sol(String s,String r,int i1,int i2,int[][] dp){
+        if(i1<0 || i2<0)return dp[i1+1][i2+1]= 0;
+        if(dp[i1+1][i2+1]!=-1)return dp[i1+1][i2+1];
+        int ans=0;
+        if(s.charAt(i1)==r.charAt(i2)){
+            ans=1+sol(s,r,i1-1,i2-1,dp);
+        }else{
+            ans=Math.max(sol(s,r,i1-1,i2,dp),sol(s,r,i1,i2-1,dp));
+        }
+        return dp[i1+1][i2+1]=ans;
+    }
 }
