71b0ce7ce97b68fddb5bb2de8c2d992d737c7ebf
Yashika Goel <79274294+Yashika1818@users.noreply.github.com>
2023-07-25 13:55:27 +0530

    Time: 35 ms (44.38%), Space: 48.3 MB (76.38%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -4,17 +4,25 @@ class Solution {
         for(int[] d:dp){
             Arrays.fill(d,-1);
         }
-        return memoization(text1,text2,0,0,dp);
-    }
-    public int memoization(String text1,String text2,int i,int j,int[][] dp){
-        if(i>=text1.length() || j>=text2.length())return dp[i][j]=0; //base cond
-        if(dp[i][j]!=-1)return dp[i][j];
-        int len=0;
-        if(text1.charAt(i)==text2.charAt(j))
-            len=1+memoization(text1,text2,i+1,j+1,dp);
-        else
-            len=Math.max(memoization(text1,text2,i+1,j,dp),memoization(text1,text2,i,j+1,dp));
         
-        return dp[i][j]=len;
+        for(int i=0;i<=text1.length();i++){
+            dp[i][text2.length()]=0;
+        }
+        for(int j=0;j<=text2.length();j++){
+            dp[text1.length()][j]=0;
+        }
+        for(int i=text1.length()-1;i>=0;i--){
+            for(int j=text2.length()-1;j>=0;j--){
+                int len=0;
+        if(text1.charAt(i)==text2.charAt(j))
+            len=1+dp[i+1][j+1];
+        else
+            len=Math.max(dp[i+1][j],dp[i][j+1]);
+        
+           dp[i][j]=len;
+            }
+        }
+        return dp[0][0];
     }
+
 }
