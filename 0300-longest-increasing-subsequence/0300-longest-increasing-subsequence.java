3506849d3065e5f5d63dfa586231ca3344bd627b
Yashika Goel <79274294+Yashika1818@users.noreply.github.com>
2023-07-28 14:52:51 +0530

    Time: 175 ms (17.79%), Space: 94.9 MB (17.18%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -4,17 +4,21 @@ class Solution {
         for(int[] d:dp){
             Arrays.fill(d,-1);
         }
-        return recursion(nums,0,-1,dp);
-    }
-    public int recursion(int[] nums,int i,int p,int[][] dp){
-        if(i==nums.length)return dp[i][p+1]=0;
-        if(dp[i][p+1]!=-1)return dp[i][p+1];
-        int take=Integer.MIN_VALUE;
-        if(p==-1 || nums[p]<nums[i]){
-            take=1+recursion(nums,i+1,i,dp);
+        for(int p=-1;p<nums.length;p++){
+            dp[nums.length][p+1]=0;
         }
-        int notTake=recursion(nums,i+1,p,dp);
+        for(int i=nums.length-1;i>=0;i--){
+            for(int p=nums.length-1;p>=-1;p--){
+                int take=Integer.MIN_VALUE;
+        if(p==-1 || nums[p]<nums[i]){
+            take=1+dp[i+1][i+1];
+        }
+        int notTake=dp[i+1][p+1];
         
-        return dp[i][p+1]=Math.max(take,notTake);
+         dp[i][p+1]=Math.max(take,notTake);
+            }
+        }
+        return dp[0][0];
     }
+
 }
