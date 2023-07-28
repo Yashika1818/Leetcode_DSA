3f681f28046b3f3cfd57754a949c8eef57e1fbce
Yashika Goel <79274294+Yashika1818@users.noreply.github.com>
2023-07-28 14:37:48 +0530

    Time: 336 ms (15.80%), Space: 95.6 MB (12.96%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,37 +1,20 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int[][] dp=new int[nums.length+1][nums.length+1];
+        int[][] dp=new int[nums.length+1][nums.length+2];
         for(int[] d:dp){
             Arrays.fill(d,-1);
         }
-        for(int p=-1;p<nums.length;p++){
-            dp[nums.length][p+1]=0;
-        }
-        for(int i=nums.length-1;i>=0;i--){
-            for(int p=i-1;p>=-1;p--){
-                int take=0;
-                if(p==-1 || nums[p]<nums[i]){
-                    take=1+dp[i+1][i+1];
-                }
-                int nottake=dp[i+1][p+1];
-
-                 dp[i][p+1]=Math.max(take,nottake);
-                
-            }
-        }
-        return dp[0][0];
+        return recursion(nums,0,-1,dp);
     }
-    
-//     public int sol(int i,int p,int[] nums,int[][] dp){
-//         if(i==nums.length)return dp[i][p+1]=0;
-//         if(dp[i][p+1]!=-1)return dp[i][p+1];
-//         int take=0;
-//         if(p==-1 || nums[p]<nums[i]){
-//             take=1+sol(i+1,i,nums,dp);
-//         }
-//         int nottake=sol(i+1,p,nums,dp);
-            
-//             return dp[i][p+1]=Math.max(take,nottake);
+    public int recursion(int[] nums,int i,int p,int[][] dp){
+        if(i==nums.length)return dp[i][p+1]=0;
+        if(dp[i][p+1]!=-1)return dp[i][p+1];
+        int take=Integer.MIN_VALUE;
+        if(p==-1 || nums[p]<nums[i]){
+            take=1+recursion(nums,i+1,i,dp);
+        }
+        int notTake=recursion(nums,i+1,p,dp);
         
-//     }
+        return dp[i][p+1]=Math.max(take,notTake);
+    }
 }
